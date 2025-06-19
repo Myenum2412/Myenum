0c608409a78c7d5de37df5cfa4dc3219531ed60c
Myenum <myenumam@gmail.com>
2025-06-19 15:53:39 +0530

    Add files via upload

---
+++ assets.ts
@@ -0,0 +1,160 @@
+import banner1 from "./homepage/banner1.png";
+import banner2 from "./homepage/banner2.png";
+import banner3 from "./homepage/banner3.png";
+import AboutusImg from "./homepage/about.png";
+import studio1 from "./studio/Pics-1.jpg";
+import studio2 from "./studio/Pics-2.jpg";
+import studio3 from "./studio/Pic-3.jpg";
+import studio4 from "./studio/Pic-4.jpg";
+import studio5 from "./studio/Pic-5.jpg";
+import studio6 from "./studio/Pics-6.jpg";
+import sale from "./homepage/sale.jpg";
+export const Banners = [
+  {
+    id: 1,
+    image: banner1,
+    head: "Digital Marketing",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, quae.",
+  },
+  {
+    id: 2,
+    image: banner2,
+    head: "Web Development",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, quae.",
+  },
+  {
+    id: 3,
+    image: banner3,
+    head: "SEO Optimization",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, quae.",
+  },
+];
+
+export const products = [
+  {
+    title: "Moonbeam",
+    link: "",
+    thumbnail: studio1,
+  },
+  {
+    title: "Cursor",
+    link: "",
+    thumbnail: studio2,
+  },
+  {
+    title: "Rogue",
+    link: "",
+    thumbnail: studio3,
+  },
+
+  {
+    title: "Editorially",
+    link: "",
+    thumbnail: studio4,
+  },
+  {
+    title: "Editrix AI",
+    link: "",
+    thumbnail: studio5,
+  },
+  {
+    title: "Pixel Perfect",
+    link: ".quest",
+    thumbnail: studio6,
+  },
+
+  {
+    title: "Algochurn",
+    link: "",
+    thumbnail: studio1,
+  },
+  {
+    title: "Aceternity UI",
+    link: ".com",
+    thumbnail: studio2,
+  },
+  {
+    title: "Tailwind Master Kit",
+    link: "",
+    thumbnail: studio3,
+  },
+  {
+    title: "SmartBridge",
+    link: "",
+    thumbnail: studio4,
+  },
+  {
+    title: "Renderwork Studio",
+    link: "",
+    thumbnail: studio5,
+  },
+
+  {
+    title: "Creme Digital",
+    link: "",
+    thumbnail: studio6,
+  },
+  {
+    title: "Golden Bells Academy",
+    link: "",
+    thumbnail: studio1,
+  },
+  {
+    title: "Invoker Labs",
+    link: "",
+    thumbnail: studio2,
+  },
+  {
+    title: "E Free Invoice",
+    link: "",
+    thumbnail: studio3,
+  },
+];
+
+export { AboutusImg, sale };
+
+
+export const testimonials = [
+  {
+    quote:
+      "Coming Soon...",
+    name: "Radisson Hotel Salem",
+    designation: "www.radissonhotels.com/en-us/hotels/radisson-salem",
+    src: "",
+  },  
+  {
+    quote: 
+      "Established in 1990 in Chinthamaniyur, Salem, Bds Handlooms is a leading silk saree manufacturer and wholesaler known for quality products, customer satisfaction, and a strong local presence with plans to expand further.",
+    name: "BDS Handlooms / BDS ஹேண்ட்லூம்ஸ்",
+    designation: "BDS Handlooms / BDS ஹேண்ட்லூம்ஸ்",
+    src: "",
+  },
+  {
+    quote:
+    "Startup Newgen is a global, innovative enterprise delivering safe, secure, and professional packing solutions across B2B and C2C sectors, with a commitment to quality, client satisfaction, and a strong presence in countries worldwide.",
+    name: "Air Winch International Courie and Cargo",
+    designation: "airwinchinternational.com",
+    src: "https://qsuv73ya7y.ufs.sh/f/fNtsXLDRapsO1xdpJONcY7IXn054ujZxJdN8PvKpEkbMFRwC",
+  },
+  {
+    quote: 
+    "Sivaraj is a luxury hotel in Salem offering exceptional service, rich heritage, and premium comfort near the bus stand and railway station",
+    name: "Sivarajinn",
+    designation: "sivarajinn.com",
+    src: "https://qsuv73ya7y.ufs.sh/f/fNtsXLDRapsOpMA6GwFTJaBvW0UrV4FlS9dCcNzD8oxZfPRY",
+  },
+  {
+    quote:
+    "They provide exceptional dental care, led by award-winning Dr. MohanBabu, specializing in Root Canal and Cosmetic Dentistry with advanced Microscope Enhanced Treatments—ranked among Salem’s top 3 clinics since 2019.",
+    name: "VAIRAM DENTAL CARE",
+    designation: "vairamdentalcare.whitecoats.com",
+    src: "https://qsuv73ya7y.ufs.sh/f/fNtsXLDRapsOPgaU0ObVvcxUOXS6fuZMJkGqlbwFDKhgjzEr",
+  },
+  {
+    quote:
+    "Huma Spices brings you the finest handpicked spices, packed with rich flavor, authenticity, and natural health benefits.",
+    name: "Huma Spices",
+    designation: "humaspices.com",
+    src: "https://qsuv73ya7y.ufs.sh/f/fNtsXLDRapsONM0ywgd5Rriq1SekcaXEzPy2FOswQTbNULMj",
+  },
+]
