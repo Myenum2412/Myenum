1f74da32f4667bcce8daa20874912e190c543e06
Myenum <myenumam@gmail.com>
2025-06-24 21:25:27 +0530

    Delete next-sitemap.config.js

--- next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://www.myenum.in/sitemap.xml', // change to your real domain
-    generateRobotsTxt: true,
-  }
-  
